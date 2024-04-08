13487e9f258ed7cf48dcb6f6b62866487244b7da
CJJJX <2873180579@qq.com>
2024-04-08 12:24:15 +0800

    add 函数柯里化

---
+++ js/函数柯里化.js
@@ -0,0 +1,21 @@
+function curry(fn){
+    return function curried(...args){
+        //当收集的参数达到原函数的参数个数时，执行原函数
+        if(args.length >= fn.length){
+            return fn.apply(this,args)       
+        }else{
+            //否则继续收集参数
+            return function(...moreArgs){
+                return curried.apply(this,args.concat(moreArgs))
+            }
+        }
+    }
+}
+let sum = function(a,b,c){
+    console.log(a+b+c)
+}
+let curried = curry(sum)
+let fn1 = curried(1)
+let fn2 = fn1(2)
+fn2(3)
+curried(1)(2)(3)
