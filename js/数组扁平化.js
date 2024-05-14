7992c74954aa76fa976263ac6e4393dd43f95faa
CJJJX <2873180579@qq.com>
2024-05-15 00:20:06 +0800

    数组扁平化更新实现

--- js/数组扁平化.js
+++ js/数组扁平化.js
@@ -1,20 +1,54 @@
 const arr = [
-    [1,2,9],
-    [2,3,3,2],
-    [3,4,5,[6,7,9]],
+    [1, 2, 9],
+    [2, 3, 3, 2],
+    [3, 4, 5, [6, 7, 9]],
     0
 ]
 // 不可以用箭头函数写法,这里箭头函数this会指向window
-Array.prototype.myFlatten = function(){
+Array.prototype.myFlatten = function () {
     let res = []
-    for(let item of this){
-        if(Array.isArray(item)){
-           res = res.concat(item.myFlatten())
-        }else{
+    for (let item of this) {
+        if (Array.isArray(item)) {
+            res = res.concat(item.myFlatten())
+        } else {
             res.push(item)
         }
     }
     return res
 }
+// 2024/05/14优化
+Array.prototype.myFlatten = function (depth = 1) {
+    // 创建一个空数组存储结果
+    let res = []
+    // 递归函数，用于扁平化数组  
+    function flatten(array, depth) {
+        let tempResult = [];
+
+        for (let item of array) {
+            // 如果当前深度大于0且当前项是数组，则递归调用flatten  
+            if (depth > 0 && Array.isArray(item)) {
+                tempResult.push(...flatten(item, depth - 1)); // 使用扩展运算符将结果添加到tempResult中  
+            } else {
+                // 否则，将当前项添加到tempResult数组中  
+                tempResult.push(item);
+            }
+        }
+        // 返回扁平化的数组  
+        return tempResult;
+    }
+
+    // 调用递归函数，并将结果添加到result数组中  
+    res.push(...flatten(this, depth));
+
+    // 返回结果数组  
+    return res;
+}
 // 用map实现
-console.log(arr.myFlatten())
+console.log('emm', arr.myFlatten())
+// arr.toString()方法也能将数组拍平
+console.log(arr.toString().split(',').map(Number))
+// 用正则实现
+console.log(JSON.parse('[' + JSON.stringify(arr).replace(/\[|\]/g, '') + ']'))
+// 用reduce实现
+console.log(arr.reduce((pre, cur) => pre.concat(Array.isArray(cur) ? cur.myFlatten() : cur), []))
+// 用flat API
